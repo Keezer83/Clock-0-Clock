37581073281a4b0ed5c1d5027eecf33ac4d18fe2
Keezer83 <126595440+Keezer83@users.noreply.github.com>
2023-03-12 18:40:09 -0300

    added arrays for months and days of week

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,33 @@
 let d = new Date();
-document.getElementById("todaysDate").innerHTML = d;
-//console.log(d);
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const currentMonth = months[d.getMonth()];
+const currentDay = days[d.getDay()];
+const dayOfMonth = d.getDate();
+const currentYear = d.getFullYear();
+document.getElementById("todaysDate").innerHTML =
+  currentDay + " " + currentMonth + " " + dayOfMonth + " " + currentYear;
+console.log(currentMonth, currentDay, dayOfMonth, currentYear);
