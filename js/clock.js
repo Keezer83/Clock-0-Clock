50187b0c219c0e40f8501e2f7a6a7e1e4ea21a03
Keezer83 <126595440+Keezer83@users.noreply.github.com>
2023-03-15 07:00:54 -0300

    got the clock ticking

--- js/clock.js
+++ js/clock.js
@@ -1,76 +1,88 @@
-let d = new Date();
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+function clock() {
+  let d = new Date();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-if (d.getHours() < 12) {
-  document.getElementById("greeting").innerHTML =
-    "Good Morning Based on your relative position on the third planet from your solar systems central star!!";
-} else if (d.getHours() > 12 && d.getHours() < 17) {
-  document.getElementById("greeting").innerHTML =
-    "Good Afternoon Based on your relative position on the third planet from your solar systems central star!!";
-} else {
-  document.getElementById("greeting").innerHTML =
-    "Good Evening Based on your relative position on the third planet from your solar systems central star!!";
-}
-
-const currentMonth = months[d.getMonth()];
-const currentDay = days[d.getDay()];
-const dayOfMonth = d.getDate();
-const currentYear = d.getFullYear();
-let currentHour = d.getHours();
-let currentMins = d.getMinutes();
-let currentSecs = d.getSeconds();
-document.getElementById("todaysDate").innerHTML =
-  currentDay + " " + currentMonth + " " + dayOfMonth + " " + currentYear;
-//console.log(currentMonth, currentDay, dayOfMonth, currentYear);
-const mornEve = amPm(currentHour);
-
-function amPm(currentHour) {
-  if (currentHour < 12) {
-    return "AM";
+  if (d.getHours() < 12) {
+    document.getElementById("greeting").innerHTML =
+      "Good Morning Based on your relative position on the third planet from your solar systems central star!!";
+  } else if (d.getHours() > 12 && d.getHours() < 17) {
+    document.getElementById("greeting").innerHTML =
+      "Good Afternoon Based on your relative position on the third planet from your solar systems central star!!";
   } else {
-    return "PM";
+    document.getElementById("greeting").innerHTML =
+      "Good Evening Based on your relative position on the third planet from your solar systems central star!!";
   }
-}
 
-if (currentHour < 10) {
-  currentHour = "0" + d.getHours();
-} else if (currentHour > 12) {
-  currentHour = d.getHours() - 12;
-}
+  let currentMonth = months[d.getMonth()];
+  let currentDay = days[d.getDay()];
+  let dayOfMonth = d.getDate();
+  let currentYear = d.getFullYear();
+  let currentHour = d.getHours();
+  let currentMins = d.getMinutes();
+  let currentSecs = d.getSeconds();
 
-if (currentMins < 10) {
-  currentMins = "0" + d.getMinutes();
-}
+  if (dayOfMonth === 1 || dayOfMonth === 21 || dayOfMonth === 31) {
+    dayOfMonth = `${dayOfMonth}st`;
+  } else if (dayOfMonth === 2 || dayOfMonth === 22) {
+    dayOfMonth = `${dayOfMonth}nd`;
+  } else if (dayOfMonth === 3 || dayOfMonth === 23) {
+    dayOfMonth = `${dayOfMonth}rd`;
+  } else {
+    dayOfMonth = `${dayOfMonth}th`;
+  }
 
-if (currentSecs < 10) {
-  currentSecs = "0" + d.getSeconds();
-}
-function currentTime() {
+  document.getElementById("todaysDate").innerHTML =
+    currentDay + " " + currentMonth + " " + dayOfMonth + " " + currentYear;
+  //console.log(currentMonth, currentDay, dayOfMonth, currentYear);
+  const mornEve = amPm(currentHour);
+
+  function amPm(currentHour) {
+    if (currentHour < 12) {
+      return "AM";
+    } else {
+      return "PM";
+    }
+  }
+
+  if (currentHour < 10) {
+    currentHour = "0" + d.getHours();
+  } else if (currentHour > 12) {
+    currentHour = d.getHours() - 12;
+  }
+
+  if (currentMins < 10) {
+    currentMins = "0" + d.getMinutes();
+  }
+
+  if (currentSecs < 10) {
+    currentSecs = "0" + d.getSeconds();
+  }
+
   let time = (document.getElementById("todaysTime").innerHTML =
     currentHour + ":" + currentMins + ":" + currentSecs + " " + mornEve);
-  console.log(time);
 }
-//setInterval(currentTime, 1000);
+setInterval(clock, 1000);
+clock();
